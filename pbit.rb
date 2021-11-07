d2a36ebf650f1c7da74513b2e7ef93a075b6c4ee
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-07 19:38:50 +0900

    chore(rename):

--- pbit.rb
+++ pbit.rb
@@ -1,12 +1,12 @@
-class Bit < Formula
+class Pbit < Formula
   desc "Put binary number"
-  homepage "https://github.com/tkomatsu/bit"
-  url "https://github.com/tkomatsu/bit/releases/download/v1.1/bit"
+  homepage "https://github.com/tkomatsu/pbit"
+  url "https://github.com/tkomatsu/pbit/releases/download/v1.1/pbit"
   sha256 "ed741f53f2045e74e5d4cade9bfa6d7a4ae272e57242f1359b0dc2b763ef6d5e"
   license "MIT"
 
   def install
-    bin.install "bit"
+    bin.install "pbit"
   end
 
   test do
