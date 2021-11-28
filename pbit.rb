3a6ba393981000babe8f4640d714b5bac45aef52
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-28 18:02:53 +0900

    udpate

--- pbit.rb
+++ pbit.rb
@@ -1,11 +1,11 @@
 class Pbit < Formula
   desc "Put binary number"
   homepage "https://github.com/tkomatsu/pbit"
-  url "https://github.com/tkomatsu/pbit/releases/download/v1.3/pbit"
+  url "https://github.com/tkomatsu/pbit/releases/download/v1.4/pbit_macos_amd64"
   # For integrity and security, we verify the hash (`openssl dgst -sha1 <FILE>`)
   # You may also use sha256 if the software uses sha256 on their homepage. Do not use md5.
   # Either generate the sha locally or leave it empty & `brew install` will tell you the expected.
-  sha256 "01b9823ad7744f7d5c813b8bea0f37de0a168a5de9dd8176d1975879c6211402"
+  sha256 "045456ecb3b97e8c80ba3e74a58e3b8e59b2b8b4e253d696649aef75551cce28"
   license "MIT"
 
   def install
